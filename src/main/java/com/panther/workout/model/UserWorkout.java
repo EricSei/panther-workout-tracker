43f5d75744452bd483d3c9b1cbf6f67aa86f5c48
Gavin Liburd <GavinLiburd901@gmail.com>
2023-06-07 07:55:47 -0400

    Added Workout and UserWorkout models and repositories

---
+++ src/main/java/com/panther/workout/model/UserWorkout.java
@@ -0,0 +1,99 @@
+package com.panther.workout.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Date;
+import java.util.Objects;
+
+public class UserWorkout implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    Integer userId;
+
+    @ManyToOne
+    @JoinColumn(name = "workout_id", referencedColumnName = "id")
+    Integer workoutId;
+
+    @Column(name = "workout_date")
+    Date workoutDate;
+
+    @Column(name = "reps")
+    Integer reps;
+
+    public UserWorkout(Integer id, Integer userId, Integer workoutId, Date workoutDate, Integer reps) {
+        this.id = id;
+        this.userId = userId;
+        this.workoutId = workoutId;
+        this.workoutDate = workoutDate;
+        this.reps = reps;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getWorkoutId() {
+        return workoutId;
+    }
+
+    public void setWorkoutId(Integer workoutId) {
+        this.workoutId = workoutId;
+    }
+
+    public Date getWorkoutDate() {
+        return workoutDate;
+    }
+
+    public void setWorkoutDate(Date workoutDate) {
+        this.workoutDate = workoutDate;
+    }
+
+    public Integer getReps() {
+        return reps;
+    }
+
+    public void setReps(Integer reps) {
+        this.reps = reps;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserWorkout that = (UserWorkout) o;
+        return Objects.equals(id, that.id) && Objects.equals(userId, that.userId) && Objects.equals(workoutId, that.workoutId) && Objects.equals(workoutDate, that.workoutDate) && Objects.equals(reps, that.reps);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, userId, workoutId, workoutDate, reps);
+    }
+
+    @Override
+    public String toString() {
+        return "UserWorkout{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", workoutId=" + workoutId +
+                ", workoutDate=" + workoutDate +
+                ", reps=" + reps +
+                '}';
+    }
+}
---
+++ src/main/java/com/panther/workout/repository/WorkoutRepository.java
@@ -0,0 +1,10 @@
+package com.panther.workout.repository;
+
+
+import com.panther.workout.model.Workout;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WorkoutRepository extends JpaRepository<Workout, Integer> {
+}
